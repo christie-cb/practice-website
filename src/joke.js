42c70d66a3089c880d44e1ff6a5efa8c3c25304f
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-15 07:46:39 +0100

    Added required headers to api call

--- src/joke.js
+++ src/joke.js
@@ -1,5 +1,10 @@
 function getApiData() {
-  return fetch("https://icanhazdadjoke.com")
+  return fetch("https://icanhazdadjoke.com", {
+      method: 'GET',
+      headers: {
+          'Accept': 'application/json'
+      }
+  })
     .then(response => {
       if (!response.ok) throw Error(response.statusText);
       return response.json();

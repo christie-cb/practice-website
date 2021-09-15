48aabb6ad26cb7f5b4e3241fd078fe008ede2149
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-15 07:39:49 +0100

    Added a test for joke.js which verifies fetch is called.

--- src/joke.js
+++ src/joke.js
@@ -1,2 +1,10 @@
-const subtract = (a,b) => a - b;
-module.exports = subtract;
+function getApiData() {
+  return fetch("https://icanhazdadjoke.com")
+    .then(response => {
+      if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })
+    .then(json => json);
+}
+
+module.exports = { getApiData };
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,2 +1,35 @@
-const subtract = require('../joke');
-test('subtract 2 - 1 to equal 1', () => {expect(subtract(2, 1)).toBe(1);});
+const { getApiData } = require('../joke');
+
+beforeAll(() => {
+    require("whatwg-fetch");
+});
+
+describe("Jokes API", () => {
+  test("it returns a joke object", async () => {
+    const expected = {
+        "id": "R7UfaahVfFd",
+        "joke": "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.",
+        "status": 200
+    };
+    mockFetch(expected);
+    const json = await getApiData();
+    expect(json).toMatchObject(expected);
+    console.log(json);
+    unMockFetch();
+  });
+});
+
+function mockFetch(expected) {
+    global.fetch = jest.fn().mockImplementation(() => {
+        const fetchResponse = {
+            ok: true,
+            json: () => Promise.resolve(expected)
+        };
+        return Promise.resolve(fetchResponse);
+    });
+}
+
+function unMockFetch() {
+    global.fetch.mockClear();
+    delete global.fetch;
+}
